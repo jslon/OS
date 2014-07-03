d34543ae4ade38a66fd289ea72f9fe825f3d3786
yonancano <yonan.cano@gmail.com>
2014-07-03 17:29:41 -0600

    Update Memoria.c

--- Memoria.c
+++ Memoria.c
@@ -11,9 +11,8 @@
 
 
 /**
- <#Memoria() es uma especie de "Constructor" que permite inicializar variables#>
- <#Modifica: Inicializa la memoria, los archivos, y otras variables requeridas#>
- @param  <# description#>
+ Memoria() es una especie de "Constructor" que permite inicializar variables.
+ Modifica: Inicializa la memoria, los archivos, y otras variables requeridas.
  */
 void Memoria(){
     
@@ -35,9 +34,9 @@ void Memoria(){
 }
 
 /**
- <#Obtiene los caracteres de pantalla y los agrega a memoria#>
- <#Modifica: La memoria y la tabla de archivos#>
- @param  <# description#>
+ Obtiene los caracteres de pantalla y los agrega a memoria.
+ Modifica: La memoria y la tabla de archivos.
+ @param datosProc son los caracteres que se digitan en pantalla, size indica el tamaño del la pagina del archivo, inicio indica el inicio del archivo
  */
 void asignar(unsigned char * datosProc, int size, int inicio /*int hueco*/){
     int j=0;
@@ -51,9 +50,9 @@ void asignar(unsigned char * datosProc, int size, int inicio /*int hueco*/){
 }
 
 /**
- <#Pemirte almacenar los archivos en la tabla de archivo#>
- <#Modifica: La tabla de archivos#>
- @param  <#EL inicio corresponde al numero de archivo que corresponde agregar#>
+ Pemirte almacenar los archivos en la tabla de archivo.
+ Modifica: La tabla de archivos.
+ @param  EL inicio corresponde al numero de archivo que corresponde agregar.
  */
 void agregarATabla(int inicio){
     if (numProcs < 10) {
@@ -74,11 +73,11 @@ void liberarMemoria(int numeroArchivo){
 
 
 /**
- Despliega el archivo almacenado en memoria
- @param noArchivo Es el numero que corresponde a la tupla en la tabla de procesos
+ Despliega el archivo almacenado en memoria.
+ @param noArchivo Es el numero que corresponde a la tupla en la tabla de procesos.
  */
 void open(){
-    char *memoriaPtr = &memoria[numArchivo*tamanoDePagina];
+    char *memoriaPtr = &memoria[numArchivo*tamanoDePagina];  //es un puntero a memoria donde esta el archivo
     int inicio  = archivos[numArchivo*tamanoDePagina];
     
     for (int i = inicio; i < inicio+tamanoDePagina; i++) {
